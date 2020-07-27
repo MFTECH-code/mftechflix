57f9531d46e910b0e990e4cd4f2de482d978c308
Matheus <matheussilvafeitosa78@gmail.com>
2020-07-27 13:21:56 -0300

    Primeiro Commit

---
+++ src/components/Menu/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Logo from '../../assets/img/Logo.png'
+import './Menu.css'
+import Button from './components/ButtonLink';
+
+function Menu() {
+    return (
+        <nav className = "Menu">
+            <a href = "/" >
+            <img className = "Logo" src = {Logo} alt = "MFtechFlix logo" />
+            </a>
+            <Button as = "a" className = "ButtonLink" href = "/">
+                Novo Video
+            </Button>
+        </nav>
+    );
+}
+
+export default Menu; // Exportando nosso componente
